5e3c8aacb2760f98ef9de5d37b70a5d886fd518b
Kaue <130503651+KaueBulhoes@users.noreply.github.com>
2024-09-24 18:29:48 -0300

    feat: added list and components

---
+++ src/containers/ListaDeTarefas/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const Container = styled.main`
+  padding: 0 40px;
+  height: 100vh;
+  overflow-y: scroll;
+`
+// Aqui usamos a estratégia de deixar apenas o conteúdo principal rolando, com 100% da viewport, e usamos scroll no eixo-y como estratégia para rolamento.
